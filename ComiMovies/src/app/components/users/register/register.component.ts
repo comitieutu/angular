9fd8fbd59c771dc7a81b8bd80a2ab8da43dda30c
comitieutu <lecomineko@gmail.com>
2019-03-05 23:10:17 +0700

    api_login

---
+++ ComiMovies/src/app/components/users/register/register.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+
+type UserFields = 'email' | 'password';
+type FormErrors = { [u in UserFields]: string };
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  userForm: FormGroup;
+  formErrors: FormErrors = {
+    'email': '',
+    'password': '',
+  };
+  validationMessages = {
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email must be a valid email',
+    },
+    'password': {
+      'required': 'Password is required.',
+      'pattern': 'Password must be include at one letter and one number.',
+      'minlength': 'Password must be at least 4 characters long.',
+      'maxlength': 'Password cannot be more than 40 characters long.',
+    },
+  };
+
+  error: any;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
